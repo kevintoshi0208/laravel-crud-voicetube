b7d3ebd413a08e4195954234301264b33b6e75eb
Kevin Huang <kevin.huang@wakeasp.com.tw>
2021-06-24 20:33:26 +0800

    add my customer user seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,5 +17,14 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         User::factory(10)->create();
+
+        DB::table('users')
+            ->insert([
+                'name' => 'kevin.huang',
+                'email' => 'kevintoshi0208@gmail.com',
+                'password' => bcrypt('password'),
+                'email_verified_at' => now(),
+                'remember_token' => Str::random(10),
+           ]);
     }
 }
